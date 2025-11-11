6fcf5c33827d9addcb409715c6bb5f3a1a30fa98
Jai <jyai.san+acs@gmail.com>
2025-11-11 20:25:11 +0800

    Create MobileKeyBridge.lua

---
+++ Scripts/MobileKeyBridge.lua
@@ -0,0 +1,108 @@
+local MobileKeyBridgeMod = GameMain:GetMod("Jai_MobileKeyBridgeMod")
+
+local Windows = GameMain:GetMod("Windows")
+local tbWindow = Windows:CreateWindow("ModListWindow")
+
+function MobileKeyBridgeMod:OnInit()
+	local tbEventMod = GameMain:GetMod("_Event")
+	tbEventMod:RegisterEvent(g_emEvent.WindowEvent, self.OnWindowEvent, self)
+end
+
+function MobileKeyBridgeMod:OnWindowEvent(pThing, pObjs)
+	local pWnd = pObjs[0]
+	local iArg = pObjs[1]
+	if pWnd == CS.Wnd_GameMain.Instance and iArg == 1 then
+		local openButton = UIPackage.CreateObject("Jai_MobileKeyBridge", "OpenButton")
+		pWnd.UIInfo.m_MainMenu:AddChild(openButton)
+		
+		openButton:GetChild("button").onClick:Add(
+			function()
+				tbWindow:Show()
+			end
+		)
+	end
+end
+
+-- Utility to create a table/map that retains insertion order
+function MobileKeyBridgeMod:createOrderedMap()
+    local map = {
+        keys = {},
+        data = {}
+    }
+	
+	map.set = function(map, key, value)
+		if map.data[key] == nil then
+			table.insert(map.keys, key)
+		end
+		
+		map.data[key] = value
+	end
+	
+	map.get = function(map, key)
+        return map.data[key]
+    end
+	
+	map.getOrderedPairs = function(map)
+		local i = 0
+		local keys = map.keys
+		local data = map.data
+
+		local function iterator()
+			i = i + 1
+			local key = keys[i]
+			if key then
+				return key, data[key]
+			end
+		end
+
+		return iterator
+	end
+	
+	return map
+end
+
+function MobileKeyBridgeMod:register(modName, modFunction, onActivated)
+	if (type(modName) == "string" and modName ~= "" and
+		type(modFunction) == "string" and modFunction ~= "" and
+		type(onActivated) == "function"
+	) then
+		self.data = self.data or self:createOrderedMap()
+		
+		if self.data:get(modName) == nil then
+			self.data:set(modName, MobileKeyBridgeMod:createOrderedMap())
+		end
+		
+		local p = self.data:get(modName)
+		p:set(modFunction, onActivated)
+		return true
+	end
+	
+	return false
+end
+
+function tbWindow:OnInit()
+	self.window.contentPane = UIPackage.CreateObject("ModUiManager", "ModListWindow")
+	self.window.closeButton = self:GetChild("frame"):GetChild("n5")
+	self.window:Center()
+	
+	local frame = self:GetChild("frame")
+	frame.title = "Mod" .. XT("手机版快捷键桥")
+	local list = self:GetChild("list");
+	
+	for modName, p in MobileKeyBridgeMod.data:getOrderedPairs() do
+		local item = list:AddItemFromPool()
+		item:GetChild("name").text = modName
+		
+		for modFunction, onActivated in p:getOrderedPairs() do
+			local button = item:GetChild("list"):AddItemFromPool()
+			button.title = modFunction
+			button.fontsize = 16
+			button.height = 35
+			button.onClick:Add(
+				function()
+					onActivated()
+				end
+			)
+		end
+	end
+end
